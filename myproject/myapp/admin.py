887312ed4849c35214053bb83ea8ab491eab6406
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-07-25 14:52:34 +0100

    fix: fixed class model

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Project, Certificate, Skill
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}  # Optional: if you add a slug field
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('title', 'issued_by', 'date_issued')
+    list_filter = ('date_issued',)
+    search_fields = ('title', 'issued_by')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'icon_class')
+    list_editable = ('level',)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    url = models.URLField(blank=True)
+    github_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('project_detail', args=[str(self.id)])
+
+class Certificate(models.Model):
+    title = models.CharField(max_length=200)
+    issued_by = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date_issued = models.DateField()
+    certificate_url = models.URLField(blank=True)
+    image = models.ImageField(upload_to='certificates/')
+    
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    level = models.IntegerField()
+    icon_class = models.CharField(max_length=50, blank=True, help_text="Font Awesome icon class")
+    
+    def __str__(self):
+        return self.name
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Message from {self.name} - {self.subject}"
